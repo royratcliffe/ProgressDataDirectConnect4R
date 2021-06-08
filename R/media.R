2aa41ab22152f517288efc3ee369979774770b8b
Roy Ratcliffe <royratcliffe@me.com>
2021-06-08 21:31:10 +0100

    Unzip Jars

---
+++ R/media.R
@@ -0,0 +1,16 @@
+unzip.Resource1.zip <- function(exdir = tempdir()) {
+  jar <- tempfile(fileext = ".jar")
+  download.file("https://media.datadirect.com/download/files/evals/JDBC/PROGRESS_DATADIRECT_JDBC_OE_ALL.jar", jar)
+  Resource1.zip <- file.path("Disk1", "InstData", "Resource1.zip")
+  unzip(jar, files = Resource1.zip, exdir = exdir)
+  file.path(exdir, Resource1.zip)
+}
+
+#' @export
+unzip.jars <- function() {
+  exdir <- tempdir()
+  unzip(unzip.Resource1.zip(), exdir = exdir)
+  files <- list.files(file.path(exdir, "$IA_PROJECT_DIR$", "lib"), "*.jar", full.names = TRUE)
+  file.copy(files, file.path(getwd(), "inst", "java"))
+  invisible()
+}
