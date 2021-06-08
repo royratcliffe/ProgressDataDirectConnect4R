e309b39fadc90c88f36231bf205f742413b5f932
Roy Ratcliffe <royratcliffe@me.com>
2021-06-08 21:40:50 +0100

    Parameterise extract directory

--- R/media.R
+++ R/media.R
@@ -6,9 +6,14 @@ unzip.Resource1.zip <- function(exdir = tempdir()) {
   file.path(exdir, Resource1.zip)
 }
 
+#' Downloads and unzips DataDirect Connect for JDBC from Progress.
+#'
+#' Downloads the 45-megabyte DataDirect JDBC evaluation. Skips the name and
+#' e-mail requirement.
+#'
+#' @param exdir Directory to extract Jar files to; created if necessary.
 #' @export
-unzip.jars <- function() {
-  exdir <- tempdir()
+unzip.jars <- function(exdir = tempdir()) {
   unzip(unzip.Resource1.zip(), exdir = exdir)
   files <- list.files(file.path(exdir, "$IA_PROJECT_DIR$", "lib"), "*.jar", full.names = TRUE)
   file.copy(files, file.path(getwd(), "inst", "java"))
